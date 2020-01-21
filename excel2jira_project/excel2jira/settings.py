763f8f013e8a23875829b3d2e505758551cd3496
rysin <sourabh301094@gmail.com>
2020-01-21 10:59:27 +0530

    Moved Secret Key

--- excel2jira_project/excel2jira/settings.py
+++ excel2jira_project/excel2jira/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kfr*@oza*a287175i!oh1&5@kpd8)o!8hpq39@+b8nz_0tr^v1'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
